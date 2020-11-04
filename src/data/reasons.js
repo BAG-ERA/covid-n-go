1c29d07fa656db4d6973b0e84b29533269ab3d52
Maria Isabel Vergara Gallego <maisaveg1@gmail.com>
2020-11-04 18:43:03 +0100

    update icons

--- src/data/reasons.js
+++ src/data/reasons.js
@@ -1,32 +1,32 @@
 /* eslint-disable no-multi-str */
 const reasons = [
   {
-    icon: 'drink.svg',
+    icon: 'work.svg',
     name: 'travail',
     description: 'Déplacements entre le domicile et le lieu d’exercice de l’activité professionnelle ou un \
     établissement d’enseignement ou de formation, déplacements professionnels ne pouvant être différés, \
     déplacements pour un concours ou un examen.',
   }, {
-    icon: 'drink.svg',
+    icon: 'shop.svg',
     name: 'achats',
     description: 'Déplacements pour effectuer des achats de fournitures nécessaires à l\'activité professionnelle,\
      des achats de première nécessité dans des établissements dont les activités demeurent autorisées, \
      le retrait de commande et les livraisons à domicile.',
   }, {
-    icon: 'drink.svg',
+    icon: 'doctor.svg',
     name: 'sante',
     description: 'Consultations, examens et soins ne pouvant être assurés à distance et l’achat de médicaments.',
   }, {
-    icon: 'drink.svg',
+    icon: 'family.svg',
     name: 'famille',
     description: 'Déplacements pour motif familial impérieux, pour l\'assistance aux personnes \
     vulnérables et précaires ou la garde d\'enfants.',
   }, {
-    icon: 'drink.svg',
+    icon: 'handicap.svg',
     name: 'handicap',
     description: 'Déplacement des personnes en situation de handicap et leur accompagnant.',
   }, {
-    icon: 'drink.svg',
+    icon: 'hangout.svg',
     name: 'sport_animaux',
     description: 'Déplacements brefs, dans la limite d\'une heure quotidienne et dans un rayon maximal \
     d\'un kilomètre autour du domicile, liés soit à l\'activité physique individuelle des personnes, à l\'\
@@ -42,7 +42,7 @@ const reasons = [
     name: 'missions',
     description: 'Participation à des missions d\'intérêt général sur demande de l\'autorité administrative',
   }, {
-    icon: 'drink.svg',
+    icon: 'child.svg',
     name: 'enfants',
     description: 'Déplacement pour chercher les enfants à l’école et à l’occasion de leurs activités périscolaires',
   },
